d173c40e06875b66ed203eb116747ca8bc5fe274
Aaron <godlyranchdressing@gmail.com>
2017-05-12 18:51:41 -0400

    Updates

--- src/gnome-shell/sass/compile-sass.sh
+++ src/gnome-shell/sass/compile-sass.sh
@@ -5,8 +5,8 @@ COMPILED_FOLDER="compiled-css/"
 
 for distro in `cat $DISTROS`
 do
-  sassc -t expanded ${distro}/gnome-shell-${distro}.scss $COMPILED_FOLDER/$distro/gnome-shell-${distro}.css 
+sassc -t expanded ${distro}/gnome-shell-${distro}.scss $COMPILED_FOLDER/$distro/gnome-shell-${distro}.css
   for type in '-light' '-opaque'; do
-    sassc -t expanded ${distro}/gnome-shell-${distro}${type}.scss $COMPILED_FOLDER/$distro/gnome-shell-${distro}${type}.css
+sassc -t expanded ${distro}/gnome-shell-${distro}${type}.scss $COMPILED_FOLDER/$distro/gnome-shell-${distro}${type}.css
   done
 done
