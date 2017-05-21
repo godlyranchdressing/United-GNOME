1744af0e76c3c19d164b8d49dd0066310848b8a3
Juha Kukkonen <juha@juhaku.ddns.net>
2017-05-21 23:14:05 +0300

    added opensuse (not working) & fedora login theme & improved installer

--- src/login-gnome-shell/README.md
+++ src/login-gnome-shell/README.md
@@ -3,16 +3,20 @@
 ## Installing
 
 * Open your terminal and browse to folder where "gnome-shell-installer.sh" is located.
-* Run the gnome-shell-installer.sh with command **./gnome-shell-installer.sh -i**
+* Run the gnome-shell-installer.sh with command **./united-login-theme-installer.sh -i** or **./united-login-theme-installer.sh -i variant** Variant stands for variation of login themese. Currently working for ubuntu and fedora.
 * After installer wizard is completed it will ask you to reboot your system. Reason behind this is 
 that for login shell theme all the changes are taken effect only after the reboot is done. 
 
 ## Uninstalling
 
 * Open your terminal and browse to folder where "gnome-shell-installer.sh" is located.
-* Run the gnome-shell-installer.sh with command **./gnome-shell-installer.sh -u**
+* Run the gnome-shell-installer.sh with command **./united-login-theme-installer.sh -u**
 * After installer wizard is completed it will ask you to reboot your system. Reason behind this is 
 that for login shell theme all the changes are taken effect only after the reboot is done. 
+
+# Tips
+
+* If you would like to use custom wallpaper just replace the original wallpaper in themes folder and run installer again. Remember use the same name.
 
 # Need to know
 
@@ -27,3 +31,4 @@ caused by gnome-shell.css to login screen. These changes are necessary to do if 
 * Login theme works quite well. Altough there are couple of unresolved issues in login screen calendar. All the information cannot fit 
 perfectly. However this is minor thing though. It might be cause of some css styles that does not take effect in login screen and is taking 
 effect when desktop is opened.
+* Opensuse login theme will cause boot error! Do not use until some issues related to gnome shell is fixed.
---
+++ src/login-gnome-shell/united-login-theme-installer.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# Used to install / unistall UNITED Gnome login theme.
+# Currently only for Ubuntu variant.
+
+gnomeShell=/usr/share/gnome-shell
+res=$gnomeShell/gnome-shell-theme.gresource
+
+function showHelp {
+    echo "Usage. united-login-installer [ -iu ] [ variant ]. E.g. united-login-installer -i
+Used to install UNITED Gnome login theme. Can be also used to uninstall login themes.
+ -i = Install
+ -u = Uninstall
+ -h | --help | ? = Help
+ variant = Variation of login theme. E.g ubuntu, opensuse or fedora.";
+}
+
+function exists {
+    if [ ! -d $1 ]; then echo "Directory \"$1\" does not exist, exiting" && exit 1; fi;
+}
+
+
+function install {
+    echo "Installing $1 login theme...";
+    # If back up resource is found uninstall old before. 
+    if [ -f $res.bak ]; then
+        uninstall;
+    fi;
+    variation=$1;
+
+    exists "$gnomeShell";
+
+    echo "Asking sudo rights to move original theme as back up.";
+    echo "mv $res $res.bak";
+
+    sudo mv $res $res.bak 
+
+    echo "Checking does $variation folder exist in current directory.";
+    exists "`pwd`/$variation";
+
+    echo "Find compile command";
+    compile=`which glib-compile-resources`
+
+    test ${#compile} -eq 0 && echo "Compile command not found, exiting" && exist 1;
+    echo "Compile command found: $compile...";
+
+    cd "`pwd`/$variation";
+    
+    echo "Compiling gnome-shell-theme.gresource.xml...";
+    $compile gnome-shell-theme.gresource.xml
+
+    echo "Copying compiled gnome shell theme to $gnomeShell";
+    sudo cp gnome-shell-theme.gresource $gnomeShell;
+
+    rm -f gnome-shell-theme.gresource;
+
+    echo "Completed!";
+}
+
+function uninstall {
+    test ! -f $gnomeShell/gnome-shell-theme.gresource.bak && echo "Back up file does not exists, cannot proceed unistall, exiting" && exit 1;
+
+    echo "Uninstalling current theme and restoring previous theme...";
+    echo "Asking sudo rights to restore.";
+    sudo rm -f $gnomeShell/gnome-shell-theme.gresource;
+    sudo mv $res.bak $res
+
+    echo "Completed!";
+}
+
+function reboot {
+    echo "Do you wish to reboot now, changes take affect after reboot? (y/N)";
+    read allowReboot;
+    if [ "$allowReboot" == "y" ]; then
+        rebootCommand=`which reboot`;
+        test ${#rebootCommand} -eq 0 && echo "Reboot command not found, exiting" && exist 1;
+        
+        echo "Reboot command found: $rebootCommand";
+
+        sudo $rebootCommand now;
+    fi;
+
+    echo "Press alt + F2 and then type r to restart gnome shell. This makes login theme party available.";
+}
+
+test ${#1} -eq 0 && showHelp && exit 1;
+
+if [[ "$1" == "-h" || "$1" == "--help" || "$i" == "?" ]]; then showHelp && exit 1; fi;
+
+if [[ "$1" != "-"* && "$1" != "?" ]]; then showHelp && exit 1; fi;
+
+mode=${1:1:1};
+
+case $mode in
+    i)
+        echo "Installing...";
+        variant="$2";
+        if [ "$variant" == "" ]; then
+            while [ "$variant" == "" ]; do
+
+            echo "What variant you would like to use?";
+
+            read variant;
+            echo "variant $variant"
+            if [ ! -d `pwd`/$variant ]; then echo "Variant `pwd`/$variant does not exist!" && variant=""; fi;
+
+            done;
+        fi;
+        install $variant;
+        reboot;
+		;;
+	u)
+		echo "Uninstalling...";
+        uninstall;
+        reboot;
+		break
+		;;
+	*)
+		showHelp;
+		;;
+esac
+
+exit 0;
