3e83bd8ba970132096acd836506e79be6bab2a4c
godlyranchdressing <godlyranchdressing@gmail.com>
2017-07-03 18:12:10 -0400

    Couple fixes
    Closes #126 #132 #131 #128. #130 slightly addressed.
    - Gets rid of Evolution's huge icons in the toolbar

--- TODO.md
+++ TODO.md
@@ -1,28 +1,20 @@
 A roadmap/todo file for contributors and anyone curious. Also technically functions as a known issues list.
 
 ----------
-# Default button styling
-- The orange is a little loud for Ubuntu.
+# Shell
+- OSD border asset needs to be tidied up a bit.
 
-# OSD borders for Shell
-- Needs to be tidied up a bit.
+# GTK2/3
+- New window button assets?
 
 # Metacity
+- Figure out how to work this.
 
-# SCSS cleanup
+# SCSS cleanup + Compact variant
 - Use extend, loops and mixin magic to reduce the amount of semi-repeated styling. Repeated dimensions can be converted into variables that'll be affected by a new compact variable in the main SASS files that can let people with smaller displays save some screen real estate. This also leads into using **em instead of px units to better support HiDPI displays**. (I don't have one to test this out myself, unfortunately)
-
-# Assets
-- Window buttons? Unity 8's are a bit too thin and look a lot like Windows 10's. The original concept had an encircled close button so something resembling that would be ideal. Ambiance's might be worth experimenting with.
-- Shell OSD border tidying
 
 # Login theme
 - The styling for the login dialog is untested but how to go about testing it without doing the whole compile into a gresource file isn't clear. I'm not sure how it happened to begin with, but on UG 17.04, a theme's login styling worked for the logged in user's lock screen but I can't seem to get that behavior working again.
 
 # Epiphany Incognito Mode
 - Right now it follows Adwaita's styling, but customizing it doesn't _seem_ to be possible.
-
-# Potential Shell bugs / seemingly unfixable from my end
-- Some dash to dock styling has the potential to repeatedly crash the Shell when opening apps.
-- The last keyboard key touched when using the on-screen keyboard is left in the "hover" state
-which misleadingly looks as if the key is stuck or input wasn't made. Might not be possible to fix at all as a the way most DEs just seem to treat the user's finger as a mouse cursor so the last key pressed has the "cursor" still on it. Maybe this is a non-issue in Wayland.
---
+++ compile-compact.sh
@@ -0,0 +1,91 @@
+#! /bin/bash
+DISTROS="compact-distros.txt"
+COMPILED_FOLDER="sass/compiled-css"
+OUTPUT="Compiled"
+
+# GTK2 #
+DISTRO_FOLDER="src/gtk-2.0/distro-gtk-files"
+ASSETS_FOLDER="src/gtk-2.0/distro-assets"
+COMMON_ASSETS_FOLDER="src/gtk-2.0/common-assets"
+COMMON_FOLDER="src/gtk-2.0/common-files"
+###
+
+echo "Compiling and copying..."
+for distro in `cat $DISTROS`; do
+  for variant in '','' '-light','-Light' '-opaque','-Dark'; do IFS=","; set -- $variant;
+    sassc -t expanded src/gnome-shell/sass/$distro/gnome-shell-$distro${1}.scss $OUTPUT/United-${distro^}/United-${distro^}${2}/gnome-shell/gnome-shell.css
+    sassc -t expanded src/gtk-3.0/sass/$distro/gtk-$distro.scss $OUTPUT/United-${distro^}/United-${distro^}/gtk-3.0/gtk.css
+    sassc -t expanded src/gtk-3.0/sass/$distro/gtk-$distro-dark.scss $OUTPUT/United-${distro^}/United-${distro^}/gtk-3.0/gtk-dark.css
+    rm $OUTPUT/United-${distro^}/United-${distro^}/gtk-3.0/assets/* -R
+    cp src/gtk-3.0/common-assets/* $OUTPUT/United-${distro^}/United-${distro^}/gtk-3.0/assets/ -R
+    cp src/gtk-3.0/distro-assets/${distro}-assets/* $OUTPUT/United-${distro^}/United-${distro^}/gtk-3.0/assets/ -R
+  done
+
+  for variant in "" '-Dark'; do
+    rm $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/* -R
+    cp src/gnome-shell/common-assets/* $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/ -R
+    cp src/gnome-shell/distro-assets/$distro-assets/* $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/ -R
+  done
+
+  for variant in '-Dark'; do
+    sassc -t expanded src/gtk-3.0/sass/$distro/gtk-$distro-dark.scss $OUTPUT/United-${distro^}/United-${distro^}-Dark/gtk-3.0/gtk.css
+    rm $OUTPUT/United-${distro^}/United-${distro^}-Dark/gtk-3.0/assets/* -R
+    cp src/gnome-shell/common-assets-dark/* $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/ -R
+    cp src/gtk-3.0/common-assets/* $OUTPUT/United-${distro^}/United-${distro^}-Dark/gtk-3.0/assets/ -R
+    for buttons in '-minimize' '-maximize' '-close'; do
+      cp src/gtk-3.0/common-assets/titlebutton${buttons}-dark.png $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/
+      cp src/gtk-3.0/common-assets/titlebutton${buttons}-backdrop-dark.png $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/
+      cp src/gtk-3.0/distro-assets/${distro}-assets/titlebutton${buttons}-hover*.png $OUTPUT/United-${distro^}/United-${distro^}-Dark/gtk-3.0/assets/
+      cp src/gtk-3.0/distro-assets/${distro}-assets/titlebutton${buttons}-active*.png $OUTPUT/United-${distro^}/United-${distro^}-Dark/gtk-3.0/assets/
+    done
+  done
+
+  for variant in '-Darker'; do
+    sassc -t expanded src/gtk-3.0/sass/$distro/gtk-$distro-darker.scss $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/gtk.css
+    sassc -t expanded src/gtk-3.0/sass/$distro/gtk-$distro-dark.scss $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/gtk-dark.css
+    rm $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/* -R
+    cp src/gtk-3.0/common-assets/* $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/ -R
+    cp src/gtk-3.0/distro-assets/$distro-assets/* $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/ -R
+    for buttons in '-minimize' '-maximize' '-close'; do
+      cp src/gtk-3.0/common-assets/titlebutton${buttons}-dark.png $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/
+      cp src/gtk-3.0/common-assets/titlebutton${buttons}-backdrop-dark.png $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/
+      cp src/gtk-3.0/distro-assets/${distro}-assets/titlebutton${buttons}-hover*.png $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/
+      cp src/gtk-3.0/distro-assets/${distro}-assets/titlebutton${buttons}-active*.png $OUTPUT/United-${distro^}/United-${distro^}-Darker/gtk-3.0/assets/
+    done
+  done
+
+  for variant in '-Light'; do
+    cp src/gnome-shell/common-assets/* $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/ -R
+    cp src/gnome-shell/common-assets-light/* $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/ -R
+    cp src/gnome-shell/distro-assets/$distro-assets/* $OUTPUT/United-${distro^}/United-${distro^}${variant^}/gnome-shell/assets/ -R
+  done
+
+  for variant in 'dark'; do
+    for file_name in 'Dark'; do
+      cp ${DISTRO_FOLDER}/${distro}/gtkrc-${variant}-${distro} Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/gtkrc
+      cp ${COMMON_FOLDER}/main.rc Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/main.rc
+      cp ${COMMON_FOLDER}/hacks-dark.rc Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/hacks.rc
+      rm Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/assets/*
+      cp ${COMMON_ASSETS_FOLDER}-${variant}/* Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/assets/
+      cp ${ASSETS_FOLDER}/${distro}/${distro}-${variant}-assets/* Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/assets/
+    done
+  done
+  for variant in 'darker'; do
+    for file_name in 'Darker'; do
+      cp ${DISTRO_FOLDER}/${distro}/gtkrc-${variant}-${distro} Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/gtkrc
+      cp ${COMMON_FOLDER}/main.rc Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/main.rc
+      cp ${COMMON_FOLDER}/hacks.rc Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/hacks.rc
+      rm Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/assets/*
+      cp src/gtk-2.0/common-assets-light/* Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/assets/
+      cp ${ASSETS_FOLDER}/${distro}/${distro}-light-assets/* Compiled/United-${distro^}/United-${distro^}-$file_name/gtk-2.0/assets/
+    done
+  done
+  for variant in 'light'; do
+    cp ${DISTRO_FOLDER}/${distro}/gtkrc-${distro} Compiled/United-${distro^}/United-${distro^}/gtk-2.0/gtkrc
+    cp ${COMMON_FOLDER}/main.rc Compiled/United-${distro^}/United-${distro^}/gtk-2.0/main.rc
+    cp ${COMMON_FOLDER}/hacks.rc Compiled/United-${distro^}/United-${distro^}/gtk-2.0/hacks.rc
+    rm Compiled/United-${distro^}/United-${distro^}/gtk-2.0/assets/*
+    cp ${COMMON_ASSETS_FOLDER}-${variant}/* Compiled/United-${distro^}/United-${distro^}/gtk-2.0/assets/
+    cp ${ASSETS_FOLDER}/${distro}/${distro}-${variant}-assets/* Compiled/United-${distro^}/United-${distro^}/gtk-2.0/assets/
+  done
+done
